84677a4313e1d06f439ef45265a260f95fc547c4
ayush12993 <ayush2202ksr@gmail.com>
2022-07-15 05:33:38 +0530

    Google Auth

--- app/src/main/java/com/moneyyapp/task1/activity/MainActivity.java
+++ app/src/main/java/com/moneyyapp/task1/activity/MainActivity.java
@@ -1,7 +1,8 @@
-package com.moneyyapp.task1;
+package com.moneyyapp.task1.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.moneyyapp.task1.R;
 
 public class MainActivity extends AppCompatActivity {
 
